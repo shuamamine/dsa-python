18306a47aa3fcad8378a5cb957f37e21f951ec4d
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-06-12 22:43:38 +0530

    Update and rename binsort.py to binarysearch.py

---
+++ searching/binarysearch.py
@@ -0,0 +1,23 @@
+
+def binarySearch(arr, key):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < key:
+            low = mid + 1
+        elif arr[mid] > key:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+
+arr = [1, 2, 3, 4, 5]
+key = 4
+idx = binarySearch(arr, key)
+if idx == -1:
+    print("Item Not Found")
+else:
+    print(str(key), "found at index:", str(idx))
